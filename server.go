dc2dd6ac9452c513e87e254d3830f4483463286b
Beau Dobbin <beau@beaunus.com>
2018-05-28 18:22:47 +0900

    echo tutorial: Request

--- server.go
+++ server.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
+type CustomContext struct {
+	echo.Context
+}
+
+func (c *CustomContext) Foo() {
+	println("foo")
+}
+
+func (c *CustomContext) Bar() {
+	println("bar")
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c}
+			return h(cc)
+		}
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.GET("/", func(c echo.Context) error {
+		cc := c.(*CustomContext)
+		cc.Foo()
+		cc.Bar()
+		return cc.String(200, "OK")
+	})
+	e.Logger.Fatal(e.Start(":8000"))
 }
