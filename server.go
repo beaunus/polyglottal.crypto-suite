6194088c81b35473e7af515087275a0f2d961511
Beau Dobbin <beau@beaunus.com>
2018-05-29 17:43:58 +0900

    Abandon graphQL (for now)

--- server.go
+++ server.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/handler"
+	"github.com/graph-gophers/graphql-go"
 	"github.com/labstack/echo"
 )
+
+var schema *graphql.Schema
 
 func main() {
 	e := echo.New()
@@ -25,30 +25,6 @@ func main() {
 		result := caesarEncrypt(plaintext, shift)
 		return c.JSON(http.StatusOK, result)
 	})
-
-	// Schema
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
-	h := handler.New(&handler.Config{
-		Schema:   &schema,
-		Pretty:   true,
-		GraphiQL: true,
-	})
-
-	e.GET("/grizzle", echo.WrapHandler(h))
-	e.POST("/grizzle", echo.WrapHandler(h))
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
