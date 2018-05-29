e11f2d4cb1504502b19978b1e706333007debbd1
Beau Dobbin <beau@beaunus.com>
2018-05-29 15:42:34 +0900

    Write Caesar Cipher

--- server.go
+++ server.go
@@ -1,59 +1,37 @@
 package main
 
 import (
-	"html/template"
-	"io"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
 
-// User implements a simple User.
-type User struct {
-	Name  string `json:"name" form:"name" query:"name"`
-	Email string `json:"email" form:"email" query:"email"`
-}
-
-// Template contains a pointer to templates.
-type Template struct {
-	templates *template.Template
-}
-
-// Render renders the given template.
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-// Hello renders the hello template with "World"
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
-}
-
 func main() {
 	e := echo.New()
 
-	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
+	e.File("/", "public/index.html")
 
-	e.Renderer = t
-	e.GET("/hello", Hello)
-
-	e.POST("/users", func(c echo.Context) (err error) {
-		u := new(User)
-		if err = c.Bind(u); err != nil {
-			return
+	e.GET("/caesarCipher", func(c echo.Context) error {
+		message := c.QueryParam("message")
+		shift, error := strconv.Atoi(c.QueryParam("shift"))
+		if error != nil {
+			fmt.Println("Caesar Cipher", error)
 		}
-		return c.JSON(http.StatusOK, u)
-	})
-
-	e.GET("/users", func(c echo.Context) (err error) {
-		u := User{"Me", "her"}
-		// if err = c.Bind(u); err != nil {
-		// 	return
-		// }
-		return c.JSON(http.StatusOK, u)
+		result := caesarEncrypt(message, shift)
+		return c.JSON(http.StatusOK, result)
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+// caesarEncrypt is alphabet agnostic.
+// TODO: determine alphabet from input
+func caesarEncrypt(plaintext string, shift int) string {
+	runes := []rune(plaintext)
+	for i, _ := range runes {
+		runes[i] += int32(shift)
+	}
+	return string(runes)
 }
