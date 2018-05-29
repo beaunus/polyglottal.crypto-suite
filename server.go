b6c93ec8eed7af1d9af8f1f43a22bb05fa605b0f
Beau Dobbin <beau@beaunus.com>
2018-05-29 16:30:14 +0900

    Serve baloney at /grizzle in graphQL

--- server.go
+++ server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 	"github.com/labstack/echo"
 )
 
@@ -22,6 +25,30 @@ func main() {
 		result := caesarEncrypt(plaintext, shift)
 		return c.JSON(http.StatusOK, result)
 	})
+
+	// Schema
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	e.GET("/grizzle", echo.WrapHandler(h))
+	e.POST("/grizzle", echo.WrapHandler(h))
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
