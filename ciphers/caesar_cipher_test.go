d3b633a740ac71c9e9c19c7f74b5bfc0c97cfe7d
Beau Dobbin <beau@beaunus.com>
2018-05-30 11:52:13 +0900

    rename "in" to "plaintext"

--- ciphers/caesar_cipher_test.go
+++ ciphers/caesar_cipher_test.go
@@ -4,16 +4,16 @@ import "testing"
 
 func TestCaesarEncrypt(t *testing.T) {
 	cases := []struct {
-		in    string
-		shift int
-		want  string
+		plaintext string
+		shift     int
+		want      string
 	}{
 		{"abcdefghijklmnopqrstuvwxyz", -1, "`abcdefghijklmnopqrstuvwxy"},
 	}
 	for _, c := range cases {
-		got := CaesarEncrypt(c.in, c.shift)
+		got := CaesarEncrypt(c.plaintext, c.shift)
 		if got != c.want {
-			t.Errorf("CaesarEncrypt(%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("CaesarEncrypt(%q, %q) == %q, want %q", c.plaintext, c.shift, got, c.want)
 		}
 	}
 }
