ae27fd5ffcc11d6cd5cdbf4fc3da51a8edb39e97
Beau Dobbin <beau@beaunus.com>
2018-05-30 16:54:06 +0900

    Account for alphabet in caesar tests

--- ciphers/caesar_cipher_test.go
+++ ciphers/caesar_cipher_test.go
@@ -5,13 +5,14 @@ import "testing"
 func TestCaesarEncrypt(t *testing.T) {
 	cases := []struct {
 		plaintext string
+		alphabet  string
 		shift     int
 		want      string
 	}{
-		{"abcdefghijklmnopqrstuvwxyz", -1, "`abcdefghijklmnopqrstuvwxy"},
+		{"abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", -1, "zabcdefghijklmnopqrstuvwxy"},
 	}
 	for _, c := range cases {
-		got := CaesarEncrypt(c.plaintext, c.shift)
+		got := CaesarEncrypt(c.plaintext, c.alphabet, c.shift)
 		if got != c.want {
 			t.Errorf("CaesarEncrypt(%q, %q) == %q, want %q", c.plaintext, c.shift, got, c.want)
 		}
