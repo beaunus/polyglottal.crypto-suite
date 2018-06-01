4952e1b49221a2efb5d1895d119a4fb490246cd5
Beau Dobbin <beau@beaunus.com>
2018-06-01 12:00:39 +0900

    Add header for no-cache

--- server.go
+++ server.go
@@ -10,9 +10,19 @@ import (
 	"github.com/beaunus/pretty-sweet-crypto-suite/ciphers"
 )
 
+func noCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1.
+		w.Header().Set("Pragma", "no-cache")                                   // HTTP 1.0.
+		w.Header().Set("Expires", "0")                                         // Proxies.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	router := mux.NewRouter()
+	router.Use(noCache)
 	router.HandleFunc("/api/v1/caesar", ciphers.CaesarHandler).Methods("GET")
 	router.HandleFunc("/api/v1/scytale", ciphers.ScytaleHandler).Methods("GET")
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
